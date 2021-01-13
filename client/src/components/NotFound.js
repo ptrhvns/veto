ca28cfbaeaafb13bc46f0a0904812383fa3fc900
Peter Havens <peter.havens@gmail.com>
2021-01-13 09:26:02 -0700

    Add a title

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,5 +1,16 @@
+import { buildTitle } from '../lib/utils';
+import { Helmet } from 'react-helmet-async';
+
 function NotFound() {
-  return <p>The page you requested was not found.</p>;
+  return (
+    <>
+      <Helmet>
+        <title>{buildTitle('Not Found')}</title>
+      </Helmet>
+
+      <p>The page you requested was not found.</p>
+    </>
+  );
 }
 
 export default NotFound;
