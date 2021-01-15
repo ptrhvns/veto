e3f38dace507c44fbe16455b398b3e9577aa74d5
Peter Havens <peter.havens@gmail.com>
2021-01-15 14:38:57 -0700

    Improve test coverage of index view

--- server/tests/views/test_index.py
+++ server/tests/views/test_index.py
@@ -1,3 +1,37 @@
-def test_index(client):
+import os
+import os.path
+import random
+
+from veto.app_factory import CLIENT_DIR
+
+
+def build_client_path(*subpaths):
+    return os.path.join(CLIENT_DIR, *subpaths)
+
+
+def read_file_bytes(filename):
+    with open(filename, "rb") as f:
+        return f.read()
+
+
+def get_random_filename(directory):
+    return random.choice(os.listdir(directory))
+
+
+def test_index_with_no_filename(client):
     response = client.get("/")
+    file_data = read_file_bytes(build_client_path("index.html"))
     assert response.status_code == 200
+    assert response.data == file_data
+
+
+def test_index_with_filename(client):
+    subpaths = ["static", "js"]
+    directory = build_client_path(*subpaths)
+    basename = get_random_filename(directory)
+    full_path = build_client_path(directory, basename)
+    file_data = read_file_bytes(full_path)
+    relative_url = "/".join(subpaths + [basename])
+    response = client.get(f"/{relative_url}")
+    assert response.status_code == 200
+    assert response.data == file_data
