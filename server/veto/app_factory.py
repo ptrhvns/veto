800b8c6d54abdfedc0fd842334ff3f85cf28a0a0
Peter Havens <peter.havens@gmail.com>
2021-01-20 12:34:43 -0700

    Add Internal Server Error handler; test Sentry in production

--- server/veto/app_factory.py
+++ server/veto/app_factory.py
@@ -2,6 +2,7 @@ import os
 import os.path
 from pathlib import Path
 
+import werkzeug
 from flask import Flask, safe_join, send_from_directory
 
 CLIENT_DIR = str(Path(__file__).resolve().parent.parent.parent / "client" / "build")
@@ -28,6 +29,11 @@ def create_app(config=None):
     def health():
         return {"msg": "OK"}
 
+    # FIXME remove after testing Sentry.
+    @app.route("/sentry")
+    def sentry():
+        return 1 / 0  # FIXME broken on purpose
+
     # This route should be defined last. It's only used with the client
     # production build (i.e the client/build directory exists). It lets the
     # client handle routing not otherwise specified by the server.
@@ -36,5 +42,11 @@ def create_app(config=None):
     def index(filename):
         f = filename if is_client_asset(filename) else "index.html"
         return send_from_directory(CLIENT_DIR, f)
+
+    @app.errorhandler(werkzeug.exceptions.InternalServerError)
+    def internal_server_error(e):
+        return {
+            "msg": "We couldn't fulfill your request due to an unexpected error."
+        }, 500
 
     return app
