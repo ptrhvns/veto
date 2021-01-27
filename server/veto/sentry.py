fd6c236310f7e2ecb25b0d07d2fa4a3017013a68
Peter Havens <peter.havens@gmail.com>
2021-01-27 15:09:39 -0700

    Simplify message

--- server/veto/sentry.py
+++ server/veto/sentry.py
@@ -8,7 +8,7 @@ from dateutil.tz import tzlocal
 def setup_sentry():
     if os.getenv("SENTRY_ENABLED") == "yes":
         now = datetime.now(tzlocal()).strftime("%Y-%m-%d %H:%M:%S %z")
-        click.echo(f"[{now}] Enabling Sentry to track errors and performance")
+        click.echo(f"[{now}] Enabling Sentry to track errors")
         import sentry_sdk
         from sentry_sdk.integrations.flask import FlaskIntegration
 
