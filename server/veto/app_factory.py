dd4761fe55446cdcd22c88488bac319f9c5fe32b
Peter Havens <peter.havens@gmail.com>
2021-01-27 15:25:49 -0700

    Refactor

--- server/veto/app_factory.py
+++ server/veto/app_factory.py
@@ -6,20 +6,27 @@ from .error_handlers import internal_server_error
 from .views import client_asset, health, root
 
 
-def create_app(config=None):
-    app = Flask(__name__, static_folder="unused")
-
+def configure_app(app, config=None):
     app.config.from_object(AppConfig())
-
     if config:
         app.config.from_mapping(config)
 
+
+def register_routes(app):
     app.add_url_rule("/api/health", "health", health)
     app.add_url_rule("/<path:filename>", "client_asset", client_asset)
     app.add_url_rule("/", "root", root)
 
+
+def register_error_handlers(app):
     app.register_error_handler(
         werkzeug.exceptions.InternalServerError, internal_server_error
     )
 
+
+def create_app(config=None):
+    app = Flask(__name__, static_folder="unused")
+    configure_app(app, config)
+    register_routes(app)
+    register_error_handlers(app)
     return app
