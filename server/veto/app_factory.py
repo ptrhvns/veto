bf825c9be359447cecb79d9236fb99d1997fe2a2
Peter Havens <peter.havens@gmail.com>
2021-02-12 12:51:31 -0700

    Refactor code; add app setup; add comments

--- server/veto/app_factory.py
+++ server/veto/app_factory.py
@@ -2,37 +2,51 @@ from flask import Flask
 from flask_talisman import Talisman
 from werkzeug.exceptions import InternalServerError
 
-from . import views
-from .app_config import AppConfig
-from .error_handlers import internal_server_error
+from . import app_config, csrf_protection, error_handlers, views
 
 
-def config_app(app, config=None):
-    app.config.from_object(AppConfig())
+def setup_app_config(app, config=None):
+    app.config.from_object(app_config.AppConfig())
+
     if config:
         app.config.from_mapping(config)
 
 
-def init_app_extensions(app):
-    Talisman(app)
+def setup_app_extensions(app):
+    talisman_kwargs = {"content_security_policy": None} if app.debug else {}
+    Talisman(app, **talisman_kwargs)
 
 
-def add_url_rules(app):
-    app.add_url_rule("/api/health", "health", views.health)
-    app.add_url_rule("/<path:filename>", "client_asset", views.client_asset)
-    app.add_url_rule("/", "root", views.root)
+def setup_app_callbacks(app):
+    app.before_request(csrf_protection.protect_request_from_csrf)
 
 
-def register_error_handlers(app):
-    app.register_error_handler(InternalServerError, internal_server_error)
+def setup_app_url_rules(app):
+    # API routes.
+    app.add_url_rule("/api/csrf_token", "csrf_token", views.csrf_token, methods=["GET"])
+    app.add_url_rule("/api/health", "health", views.health, methods=["GET"])
+
+    # Client asset routes for single-page application. Client handles 404s.
+    app.add_url_rule(
+        "/<path:filename>", "client_asset", views.client_asset, methods=["GET"]
+    )
+    app.add_url_rule("/", "root", views.root, methods=["GET"])
+
+
+def setup_app_error_handlers(app):
+    app.register_error_handler(
+        InternalServerError, error_handlers.internal_server_error
+    )
 
 
 def create_app(config=None):
-    # Sideline static_folder to prevent route conflicts with client.
+    # Sideline static_folder to prevent client route conflicts (e.g. 404s).
     app = Flask("veto", static_folder="unused")
 
-    config_app(app, config)
-    init_app_extensions(app)
-    add_url_rules(app)
-    register_error_handlers(app)
+    setup_app_config(app, config)
+    setup_app_extensions(app)
+    setup_app_callbacks(app)
+    setup_app_url_rules(app)
+    setup_app_error_handlers(app)
+
     return app
